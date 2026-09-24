39007650346f76fd7a8ae48a6b81f9ba37ad1e85
Minahil Faisal <minahilfaisal@u172-016-107-234.wireless.sfu.ca>
2026-09-23 17:33:56 -0700

    feat: add template for labs

---
+++ projects/iat-806/lab-02/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  let canvas = createCanvas(800, 600);
+  canvas.parent("sketch-holder");
+}
+
+function draw() {
+  background(0);
+  fill(255, 182, 193);
+  circle(400, 265, 50);
+}
