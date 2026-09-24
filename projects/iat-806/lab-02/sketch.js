9c964f34fce6823af9bb79a700f31ca15301cc2e
Minahil Faisal <minahilfaisal@Minahils-MacBook-Air.local>
2026-09-23 20:50:07 -0700

    feat: lab02

--- projects/iat-806/lab-02/sketch.js
+++ projects/iat-806/lab-02/sketch.js
@@ -3,9 +3,52 @@ function setup() {
   canvas.parent("sketch-holder");
 }
 
-function draw() {
-  background("black");
-  fill(255, 182, 193);
-  stroke("white");
-  circle(400, 265, 50);
+const colors = {
+  pink: [255, 182, 193],
+  teal: [35, 132, 135],
+  black: [0, 0, 0],
+  white: [255, 255, 255]
+}
+
+let circleX = 50;
+let circleY = 400;
+let size = 50;
+let radius = size / 2;
+let speedX = 5;
+// let increment = 3;
+
+function draw() { // -> runs forever, infinite loop
+  background(colors.black);
+  fill(colors.pink);
+  stroke(colors.white);
+
+  // circleY = height / 2; // height is a variable created by p5
+  circleX += speedX; // move the circle to the right faster
+  // circleY = height / 2;
+  circleY += increment; // move the circle down as it grows
+
+  // size += increment; // increase the size of the circle
+  // circleY += increment; // move the circle down as it grows
+  radius = size / 2;
+
+  // horizontal border
+  if (circleX + radius >= width || circleX <= radius) {
+    speedX = speedX * -1; // reset the circle to the left side of the canvas
+    increment = increment * -1;
+  }
+
+  // vertical border
+  if (circleY + radius >= height || circleY <= radius) {
+    increment = increment * -1;
+  }
+
+  if (circleX >= width / 2) {
+    fill(colors.teal);
+  }
+
+  circle(circleX, circleY, size); // draw the circle
+}
+
+function mousePressed() {
+  circleX = 0; // reset the circle to the left side of the canvas
 }
--- projects/iat-806/template/sketch.js
+++ projects/iat-806/template/sketch.js
@@ -1,11 +1,17 @@
+const colors = {
+  pink: [255, 182, 193],
+  black: [0, 0, 0],
+  white: [255, 255, 255]
+}
+
 function setup() {
   let canvas = createCanvas(800, 600);
   canvas.parent("sketch-holder");
 }
 
 function draw() {
-  background("black");
-  fill(255, 182, 193);
-  stroke("white");
+  background(colors.black);
+  fill(colors.pink);
+  stroke(colors.white);
   circle(400, 265, 50);
 }
